0fb0c1bb05c78dd5b0ec435e91a6338e8c779e49
Miyamura80 <eitomiyamura@gmail.com>
2023-12-11 20:25:11 +0000

    Completed Thrust Sorting & Reduction

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # GPU-Algorithms
 Playing around with Thrust, and other GPU Algorithms
+
+# Demo Comparisons:
+
+## Sorting:
+
+`demo1.cu`
+
+```
+nvcc -O2 -gencode arch=compute_86,code=sm_86 demo.cu -o build/thrust_sort
+./build/thrust_sort
+CPU sort in 3133.77 ms
+CPU sort in 523.632 ms
+CPU sort in 525.84 ms
+CPU sort in 538.64 ms
+CPU sort in 528.767 ms
+--------------------------------------------------
+GPU sort in 6.21531 ms
+GPU sort in 7.56116 ms
+GPU sort in 7.6428 ms
+GPU sort in 7.64368 ms
+GPU sort in 7.76629 ms
+```
+
+## Sum reduction:
+
+`demo2.cu`
+
+```
+(base) eito@EitoZbook:~/GPU-Algorithms$ make run
+nvcc -O2 -gencode arch=compute_86,code=sm_86 demo2.cu -o build/thrust_sort
+./build/thrust_sort
+CPU sum: 563608529 in 27.1164 ms
+CPU sum: 563608529 in 30.1195 ms
+CPU sum: 563608529 in 29.2672 ms
+CPU sum: 563608529 in 31.0961 ms
+CPU sum: 563608529 in 31.8511 ms
+--------------------------------------------------
+GPU sum: 563608529 in 2.01526 ms
+GPU sum: 563608529 in 2.3816 ms
+GPU sum: 563608529 in 2.3094 ms
+GPU sum: 563608529 in 2.21172 ms
+GPU sum: 563608529 in 2.21746 ms
+```
+
--- demo.cu
+++ demo.cu
@@ -3,24 +3,47 @@
 #include <thrust/generate.h>
 #include <thrust/sort.h>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <iostream>
 
 int main(void)
 {
+    const size_t N = 32 << 20;
+    const int K = 5; // Number of times to repeat each operation
+    std::vector<int> h_vec_cpu(N);
+
     // generate 32M random numbers on the host
-    thrust::host_vector<int> h_vec(32 << 20);
-    thrust::generate(h_vec.begin(), h_vec.end(), rand);
+    thrust::generate(h_vec_cpu.begin(), h_vec_cpu.end(), rand);
+
+    // Run CPU sort K times
+    for (int i = 0; i < K; ++i) {
+        auto cpu_start = std::chrono::high_resolution_clock::now();
+        std::sort(h_vec_cpu.begin(), h_vec_cpu.end());
+        auto cpu_end = std::chrono::high_resolution_clock::now();
+
+        // Output the CPU performance
+        std::chrono::duration<double, std::milli> cpu_duration = cpu_end - cpu_start;
+        std::cout << "CPU sort in " << cpu_duration.count() << " ms\n";
+    }
 
     // transfer data to the device
-    thrust::device_vector<int> d_vec = h_vec;
+    thrust::device_vector<int> d_vec = h_vec_cpu;
 
+    std::cout << "--------------------------------------------------\n";
     // ensure all previous CUDA operations have completed
-    cudaDeviceSynchronize();
 
-    // sort data on the device (84M keys per second on GeForce GTX 480)
-    thrust::sort(d_vec.begin(), d_vec.end());
+    // sort data on the device K times
+    for (int i = 0; i < K; ++i) {
+        auto gpu_start = std::chrono::high_resolution_clock::now();
+        thrust::sort(d_vec.begin(), d_vec.end());
+        auto gpu_end = std::chrono::high_resolution_clock::now();
 
-    // transfer data back to host
-    thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+        // Output the GPU performance
+        std::chrono::duration<double, std::milli> gpu_duration = gpu_end - gpu_start;
+        std::cout << "GPU sort in " << gpu_duration.count() << " ms\n";
+    }
 
     return 0;
 }
